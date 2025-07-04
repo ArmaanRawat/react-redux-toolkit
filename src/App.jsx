fc1fad3375746279f56bbaa550e6fa79c7982b04
Armaan rawat <armaanrawat@Armaans-MacBook-Air.local>
2025-07-05 01:38:23 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Well this is yet another TODO app but this is for teaching and practicing the react redux toolkit.</h1>
+    </>
+  )
+}
+
+export default App
